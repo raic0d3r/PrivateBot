5141ae3d0b99e257781f6c67489585548fd76d4b
Mr Raihan <36988513+raic0d3r@users.noreply.github.com>
2022-06-19 00:41:33 +0200

    Update hackertarget.py

--- hackertarget.py
+++ hackertarget.py
@@ -1,51 +1,51 @@
-import json
-import time, getpass
-import requests
-#from urllib.request import urlopen
+import urllib.request
+import socket
+import socks
 from stem import Signal
 from stem.control import Controller
- 
-proxies = {
-    'http': 'socks5://localhost:9050',
-    'https': 'socks5://localhost:9050'
-}
- 
- 
+from time import time as timer
+from multiprocessing.dummy import Pool
+
 def renew_tor_ip():
     with Controller.from_port(port=9051) as controller:
         controller.authenticate()
         controller.signal(Signal.NEWNYM)
- 
-def hackertarget(url): 
-    for x in range(1):
-        renew_tor_ip()
-        ipAddresss = requests.get("http://api.ipify.org", proxies=proxies).text 
-        print ipAddresss        
-        ipAddress = requests.get('http://api.hackertarget.com/reverseiplookup/?q='+url, proxies=proxies).text
-  #      print ipAddress
-        print 'Current Url: '+ url    
-        if '.com' in ipAddress:          
-            print ipAddress            
-            save = open("hackertarget.txt","a")
-            save.write(ipAddress + '\n')
-            save.close()
-            #hackertargetdprm()           
-            time.sleep(1)
-        else:
-            return         
- #       time.sleep(1)
-    
-def passfor():		
-    list = open(raw_input("List : "),'r').readlines()
-    for url in list:
-    	try:
-           url = url.rstrip()    	
-           data=hackertarget(url)
-        except:
-            pass                   
 
-Passwd = getpass.getpass("Give Passwd : ")
-if "" in Passwd:
-	passfor()
-else:
-	exit()    
+def hackertarget(url):
+    try:
+        for x in range(1):
+            renew_tor_ip()
+            socks.set_default_proxy(socks.SOCKS5, "localhost",port=9050)
+            socket.socket = socks.socksocket
+            res1=urllib.request.urlopen('http://api.ipify.org', timeout=10).read().decode("utf-8")
+            res=urllib.request.urlopen('http://api.hackertarget.com/reverseiplookup/?q='+url, timeout=20).read().decode("utf-8")
+            if '.com' in res:
+                print("Scanning ==>"+url)          
+                print (res)            
+                save = open("hackertarget.txt","a")
+                save.write(res + '\n')
+                save.close()
+                #hackertargetdprm()           
+                time.sleep(1)
+            else:
+                return
+
+    except urllib.error.URLError:
+        pass
+
+    except urllib.error.HTTPError:
+        pass        
+
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
+
+    except:
+        pass
+
+def passfor():
+    url = open(input("[!] ENTER LIST OF WEBSITES : "), 'r').read().splitlines()
+    start = timer()
+    ThreadPool = Pool(100)
+    Threads = ThreadPool.map(hackertarget, url)
+
+passfor()
